4824b04354e80f67941b2b97ada233c51618f471
Tchontk <tchontk@gmail.com>
2016-10-13 19:24:32 +0200

    Express & POST

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 var express = require('express'),
-  app = express(),
-  engines = require('consolidate');
+    app = express(),
+    engines = require('consolidate'),
+    bodyParser = require('body-parser');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 // Handler for internal server errors
 function errorHandler(err, req, res, next) {
-  console.error(err.message);
-  console.error(err.stack);
-  res.status(500).render('error_template', {
-    error: err
-  });
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
 }
 
-// Le " :name " n'est pas le chemin mais l'information saisie après le slash
-// http://localhost:3000/top ==> var name = "top"
-// http://localhost:3000/top?getvar1=hello
-app.get('/:name', function (req, res, next) {
-  var name = req.params.name;
-  var getvar1 = req.query.getvar1 || 'a';
-  var getvar2 = req.query.getvar2;
-  res.render('hello', {
-    name: name,
-    getvar1: getvar1,
-    getvar2: getvar2
-  });
+app.get('/', function(req, res, next) {
+    res.render('fruitPicker', { 'fruits' : [ 'apple', 'orange', 'banana', 'peach' ] });
+});
+
+app.post('/favorite_fruit', function(req, res, next) {
+    var favorite = req.body.fruit;
+    if (typeof favorite == 'undefined') {
+        next('Please choose a fruit!');
+    }
+    else {
+        res.send("Your favorite fruit is " + favorite);
+    }
 });
 
 app.use(errorHandler);
 
-var server = app.listen(3000, function () {
-  var port = server.address().port;
-  console.log('Express server listening on port %s.', port);
+var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s.', port);
 });
